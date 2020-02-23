eb7922cd8169cf4b21fdc396f8f69f481298d605
ascheman <github@aschemann.net>
2020-02-23 13:15:51 +0000

    Initial import

---
+++ src/main/java/org/dukecon/jx/demo/jxdemo200223131533/DemoApplication.java
@@ -0,0 +1,13 @@
+package org.dukecon.jx.demo.jxdemo200223131533;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
